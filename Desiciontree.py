1115fa035ecec575cb63216032460b6740aa44ed
dkaragiannidis <37673062+dkaragiannidis@users.noreply.github.com>
2018-05-18 23:08:44 +0300

    Update Desiciontree.py

--- Desiciontree.py
+++ Desiciontree.py
@@ -1,8 +1,12 @@
+import collections
+
 import nltk
+import wordcount as wordcount
 from sklearn.model_selection import train_test_split
 from nltk.corpus import stopwords
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
+
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.tree import DecisionTreeClassifier
 import seaborn as sns
@@ -10,14 +14,13 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import confusion_matrix, classification_report
 from nltk.stem.snowball import SnowballStemmer
-import numpy
-import  string
+from collections import Counter
 import pymongo
 #ΑΝΕΒΑΣΜΑ ΑΡΧΕΙΩΝ
 client = pymongo.MongoClient('localhost', 27017)
 db = client['yelp']
 data=db["reviews"].find()
-yelp_reviews=pd.DataFrame(list(data)).sample(10000)
+yelp_reviews=pd.DataFrame(list(data)).sample(1000)
 data2=db["restaurants"].find()
 yelp_restaurant=pd.DataFrame(list(data2))
 #ΚΑΝΟΥΜΕ ΤΑ DARAFRAME ΕΝΑ ΚΑΙ ΠΕΤΑΜΕ ΤΟ STARS TOY ΡΕΣΤΟΡΑΝΤ
@@ -25,66 +28,61 @@ yelp_restaurant=yelp_restaurant.drop("stars",axis=1)
 yelp=pd.merge(yelp_reviews,yelp_restaurant,on="business_id",how='inner')
 stemmer = SnowballStemmer("english")
 yelp.shape
-
-print(yelp.head(5))
-
-yelp['text_length'] = yelp['text'].apply(len)
+yelp['text_length'] = yelp['neighborhood'].apply(len)
 yelp.head()
 yelp.info()
 yelp.describe()
+''''#plt.show()
 g = sns.FacetGrid(data=yelp, col='stars')
 g.map(plt.hist, 'text_length', bins=150)
-plt.show()
+#plt.show()
 sns.boxplot(x="stars", y="text_length", data=yelp)
-plt.show()
+#plt.show()
 stars = yelp.groupby('stars').mean()
 sns.heatmap(data=stars.corr(), annot=True)
-plt.show()
+#plt.show()'''
 yelp_class = yelp[(yelp['stars'] == 1) | (yelp['stars'] == 5)]
-print(yelp_class)
-print(yelp_class.shape)
+positive=0
+negative=0
+for n in yelp_class['stars']:
+    if n==5:
+        positive +=1
+    elif n==1:
+        negative+=1
+
+sizes=[positive,negative]
+labels =['positive','negative']
+plt.pie(sizes, labels =labels, colors = ['red','blue'], shadow = True, )
+plt.title("positive and negative crowd ")
+#plt.show()
+
+Counter = Counter(yelp_class['text'])
 
 
-X = yelp_class['text'].apply(lambda x: [stemmer.stem(y) for y in x])
+df = pd.DataFrame(columns=[yelp_class['text'],Counter.most_common()])
+df.head(10)
+''''Prepei na ginei merge me to dataframe kai na mpei sta feature gia na prosthesoume ena akomi '''
+
+X = yelp_class[["text",'address','city']].astype(str).sum(axis=1)
 y = yelp_class['stars']
 
 import string
-
-
 def text_process(text):
-    '''
-    Takes in a string of text, then performs the following:
-    1. Remove all punctuation
-    2. Remove all stopwords
-    3. Return the cleaned text as a list of words
-    '''
-
     nopunc = [char for char in text if char not in string.punctuation]
 
     nopunc = ''.join(nopunc)
 
     return [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
-print("kanei tikenize")
-bow_transformer = CountVectorizer(analyzer=text_process).fit(X)
+print("arxise tifidf")
+bow_transformer = TfidfVectorizer(analyzer=text_process).fit(X)
 print("telow vectorizer")
 len(bow_transformer.vocabulary_)
 X = bow_transformer.transform(X)
-print (X.todense())
-from sklearn.feature_extraction.text import TfidfTransformer
-tfidf = TfidfTransformer(norm="l2")
-tfidf.fit(X)
-print ("IDF:", tfidf.idf_)
-X = tfidf.transform(X)
-print (X.todense())
-print("vocabulary",)
-print('Shape of Sparse Matrix: ', X.shape)
-print('Amount of Non-Zero occurrences: ', X.nnz)
-# Percentage of non-zero values
-density = (100.0 * X.nnz / (X.shape[0] * X.shape[1]))
-print('Density: {}'.format((density)))
+
+print("telos tifidf vectorizer")
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 print("split ok")
 
 nb = MultinomialNB()
@@ -106,3 +104,16 @@ rf.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
+print("lenear regression")
+from sklearn.linear_model import LinearRegression
+
+lm = LinearRegression()
+lm.fit(X_train, y_train)
+lm.coef
+y_pred = classifier.predict(X_test)
+predictions = lm.predict(X_test)
+plt.scatter(y_test, predictions)
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+plt.xlabel('Y Test')
+plt.ylabel('Predicted Y')
